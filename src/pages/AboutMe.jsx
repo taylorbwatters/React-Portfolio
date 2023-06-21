0cb54f04c0a8518fa6e9c3cfe20d650a8f9b419a
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-21 13:57:48 -0700

    testing for redeploy

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,7 +4,7 @@ export function AboutMe() {
     return (
         <>
             <h2>About Me</h2>
-            <p> Allow me to introduce myself. I'm Taylor Watters, originally from Tucson, Arizona, but currently residing in the beautiful state of Washington alongside my wonderful husband, Sean. While my educational background began with a Bachelor's degree in Kinesiology from ASU, my journey took an exciting turn as I ventured into the world of software engineering. 
+            <p>Allow me to introduce myself. I'm Taylor Watters, originally from Tucson, Arizona, but currently residing in the beautiful state of Washington alongside my wonderful husband, Sean. While my educational background began with a Bachelor's degree in Kinesiology from ASU, my journey took an exciting turn as I ventured into the world of software engineering. 
                 Recently, I successfully completed the fullstack developer bootcamp at the University of Washington, marking a significant milestone in my career transition. When I'm not busy studying code, you can find me in the great outdoors, hiking through breathtaking trails, fishing, or engaging in friendly competition with my husband by outwitting him in board games.
                 Additionally, my furry companions, my two dogs Sam and Leia, bring endless joy and companionship to my life.</p>
             <section>
