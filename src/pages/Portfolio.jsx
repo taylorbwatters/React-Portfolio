e0a5fab770e7720e9ca98b4f62f8c056d7cd7e12
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-21 16:36:04 -0700

    fixes project images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Project } from '../components/Project';
 
 const projects = [
-    { name: '#Adulting', githubUrl: 'https://github.com/aerostokes/hashtag-adulting', deployedUrl: 'https://hashtag-adulting.herokuapp.com/', imgUrl: '/images/hashtag-adulting.png' },
-    { name: 'Work Day Scheduler', githubUrl: 'https://github.com/taylorbwatters/work-day-scheduler', deployedUrl: 'https://taylorbwatters.github.io/work-day-scheduler/', imgUrl: '/images/work-day-scheduler.png' },
-    { name: 'Note Taker', githubUrl: 'https://github.com/taylorbwatters/note-taker', deployedUrl: 'https://tay-note-taker.herokuapp.com/', imgUrl: '/images/note-taker.png' },
+    { name: '#Adulting', githubUrl: 'https://github.com/aerostokes/hashtag-adulting', deployedUrl: 'https://hashtag-adulting.herokuapp.com/', imgUrl: './images/hashtag-adulting.png' },
+    { name: 'Work Day Scheduler', githubUrl: 'https://github.com/taylorbwatters/work-day-scheduler', deployedUrl: 'https://taylorbwatters.github.io/work-day-scheduler/', imgUrl: './images/work-day-scheduler.png' },
+    { name: 'Note Taker', githubUrl: 'https://github.com/taylorbwatters/note-taker', deployedUrl: 'https://tay-note-taker.herokuapp.com/', imgUrl: './images/note-taker.png' },
 ]
 
 export function Portfolio() {
